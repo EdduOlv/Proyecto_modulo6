72160995e29c53ae325b6f58c16139acf76d74d8
EdduOlv <olivareseduardo.saez@gmail.com>
2024-08-12 01:27:32 -0400

    avance

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
 const User = require('../models/userModel')
 const bcryptsjs = require('bcryptsjs')
 const jwt = require('jsonwebtoken')
+
+
+// RECORDAR APLICAR user.findOne({}) para hacer la validacion
